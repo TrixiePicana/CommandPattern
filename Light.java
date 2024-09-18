845103a1dfeac0c89023f54e4fe9d0142279aa16
TrixiePicana <152823734+TrixiePicana@users.noreply.github.com>
2024-09-18 18:36:20 +0800

    Add files via upload

---
+++ Light.java
@@ -0,0 +1,14 @@
+package commandPattern;
+
+public class Light //Receiver class
+{
+    public void turnOn()
+    {
+        System.out.println("Light is turned ON");
+    }
+
+    public void turnOff()
+    {
+        System.out.println("Light is turned OFF");
+    }
+}
